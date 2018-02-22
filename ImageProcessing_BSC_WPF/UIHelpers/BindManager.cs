6a1ba0be95a7fa27e2cd782be84007d0025f421f
james089 <linbojun089@gmail.com>
2018-02-22 14:24:11 -0800

    combine binds and bindmanager

--- ImageProcessing_BSC_WPF/UIHelpers/BindManager.cs
+++ ImageProcessing_BSC_WPF/UIHelpers/BindManager.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.ComponentModel;
+using System;
+
 namespace ImageProcessing_BSC_WPF
 {
     public class BindManager
@@ -34,5 +36,169 @@ namespace ImageProcessing_BSC_WPF
             ML_desWidth = new BindInt();                ML_desWidth.value = 32;
             ML_desHeight = new BindInt();               ML_desHeight.value = 32;
         }
+
+        #region Binding Settings
+        public class BindString : INotifyPropertyChanged
+        {
+            public BindString() { }
+
+            private string value_ = null;
+            public string value
+            {
+                get { return value_; }
+                set
+                {
+                    if (value_ != value)
+                    {
+                        value_ = value;
+                        NotifyPropertyChanged("value");
+                    }
+                }
+            }
+
+            #region INotifyPropertyChanged Members
+
+            private void NotifyPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            #endregion
+        }
+
+        public class BindInt : INotifyPropertyChanged
+        {
+            public BindInt() { }
+
+            private int value_ = 0;
+            public int value
+            {
+                get { return value_; }
+                set
+                {
+                    if (value_ != value)
+                    {
+                        value_ = value;
+                        NotifyPropertyChanged("value");
+                    }
+                }
+            }
+
+            #region INotifyPropertyChanged Members
+
+            private void NotifyPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            #endregion
+        }
+
+        public class BindDouble : INotifyPropertyChanged
+        {
+            public BindDouble() { }
+
+            private double value_ = 0;
+            public double value
+            {
+                get { return value_; }
+                set
+                {
+                    if (value_ != value)
+                    {
+                        value_ = value;
+                        NotifyPropertyChanged("value");
+                    }
+                }
+            }
+
+            #region INotifyPropertyChanged Members
+
+            private void NotifyPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            #endregion
+        }
+
+        public class BindBool : INotifyPropertyChanged
+        {
+            public BindBool() { }
+
+            private bool value_ = false;
+            public bool value
+            {
+                get { return value_; }
+                set
+                {
+                    if (value_ != value)
+                    {
+                        value_ = value;
+                        NotifyPropertyChanged("value");
+                    }
+                }
+            }
+
+            #region INotifyPropertyChanged Members
+
+            private void NotifyPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            #endregion
+        }
+        public class GetUIVal
+        {
+            public static double getparamDouble(string vstr, string errmsg)
+            {
+                if (vstr == null || vstr.Length == 0)
+                    throw (new Exception(errmsg));
+                double val;
+                if (!double.TryParse(vstr, out val))
+                    throw (new Exception(errmsg));
+                return val;
+            }
+            public static int getparamint(string vstr, string errmsg)
+            {
+                if (vstr == null || vstr.Length == 0)
+                    throw (new Exception(errmsg));
+                int val;
+                if (!int.TryParse(vstr, out val))
+                    throw (new Exception(errmsg));
+                return val;
+            }
+            public static uint getparamuint(string vstr, string errmsg)
+            {
+                if (vstr == null || vstr.Length == 0)
+                    throw (new Exception(errmsg));
+                uint val;
+                if (!uint.TryParse(vstr, out val))
+                    throw (new Exception(errmsg));
+                return val;
+            }
+        }
+        #endregion
     }
 }
